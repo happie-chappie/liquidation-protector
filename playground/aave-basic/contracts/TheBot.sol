dbe3a7c5c5509109c55378e4ea56c59766eb49a5
happieChappie <sidharthad@protonmail.com>
2021-01-25 08:59:15 -0500

    basic aave deposit workflow with escrow contract

--- playground/aave-basic/contracts/TheBot.sol
+++ playground/aave-basic/contracts/TheBot.sol
@@ -5,35 +5,47 @@ import "./IERC20.sol";
 import "./IWETHGateway.sol";
 import "hardhat/console.sol";
 
+/*
+This is the Liquidation Protection Bot, features of this bot are
+	- This is an escrow account
+
+There is the depositor - who wants liquidity to be protected
+Core Wrapper is for 
+1 depositor
+Deposited 2 ETH
+Borrowed 1 ETH
+ETH => USDT
+Instant Liquidation
+*/
 contract TheBot {
-    address arbiter;
     address depositor;
-    address beneficiary;
     uint initialDeposit;
+
     IWETHGateway gateway = IWETHGateway(0xDcD33426BA191383f1c9B431A342498fdac73488);
     IERC20 aWETH = IERC20(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e);
 
-    constructor(address _arbiter, address _beneficiary) payable {
-        arbiter = _arbiter;
-        beneficiary = _beneficiary;
+    constructor() payable {
         depositor = msg.sender;
         initialDeposit = msg.value;
 
-		console.log(initialDeposit);
+		// console.log(initialDeposit);
+		// initally we assume that the depositor has deposited ETH and borrowed ETH
+		// depositting in the aave with the escrow contract all the eth
         gateway.depositETH{value: address(this).balance}(address(this), 0);
     }
 
     receive() external payable {}
 
     function approve() external {
-        require(msg.sender == arbiter);
+		// only the escrow account can execute the approve
+        // require(msg.sender == this.address);
 
         uint balance = aWETH.balanceOf(address(this));
-		console.log(balance);
+		// console.log(balance);
         aWETH.approve(address(gateway), balance);
 
         gateway.withdrawETH(balance, address(this));
-        payable(beneficiary).transfer(initialDeposit);
+        // payable(depositor).transfer(initialDeposit);
 
         selfdestruct(payable(depositor));
     }
--- playground/aave-basic/test/TheBot.js
+++ playground/aave-basic/test/TheBot.js
@@ -1,18 +1,21 @@
 const { assert } = require("chai");
 
-describe("TheBot", function () {
+describe("The Liquidation Protector Bot", function () {
     let theBot;
     let aWETH;
-    let arbiter;
-    let beneficiary;
     let depositor;
     let pool;
+    let balanceBefore;
+
     const deposit = ethers.utils.parseEther("2");
+
     before(async () => {
         const TheBot = await ethers.getContractFactory("TheBot");
-        [depositor, arbiter, beneficiary] = await ethers.provider.listAccounts();
-	console.log(deposit.toString());
-        theBot = await TheBot.deploy(arbiter, beneficiary, { value: deposit });
+        [depositor] = await ethers.provider.listAccounts();
+	console.log("====== The Initial depositor balance =====");
+	balanceBefore = await ethers.provider.getBalance(depositor);
+	console.log(balanceBefore.toString());
+        theBot = await TheBot.deploy({ value: deposit });
         await theBot.deployed();
         aWETH = await ethers.getContractAt("IERC20", "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e");
 	pool = await ethers.getContractAt("ILendingPool", "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9");
@@ -29,18 +32,46 @@ describe("TheBot", function () {
     });
 
     describe('after approving', () => {
+        before(async () => {
+            const depositorSigner = await ethers.provider.getSigner(depositor);
+            const thousandDays = 1000 * 24 * 60 * 60;
+            await hre.network.provider.request({
+                method: "evm_increaseTime",
+                params: [thousandDays]
+            });
+            await theBot.connect(depositorSigner).approve();
+	});
+
+        it('should payback the depositor, and it should be above the initial balance', async () => {
+            const balanceAfter = await ethers.provider.getBalance(depositor);
+	    console.log("====== The Final depositor balance =====");
+	    console.log(balanceAfter.toString());
+	    assert.isAbove(balanceAfter, balanceBefore, 'balanceAfter is strictly greater than balanceBefore');
+	    // const diff = balanceAfter.sub(balanceBefore);
+	    // assert.equal(diff.toString(), deposit.toString());
+        });
+
+    });
+    /*
+    describe('after approving', () => {
         let balanceBefore;
         before(async () => {
             balanceBefore = await ethers.provider.getBalance(beneficiary);
             arbiterBalanceBefore = await ethers.provider.getBalance(arbiter);
-	    let foo = await pool.getUserAccountData(theBot.address);
 	    console.log("===== aave pool =======");
-	    // console.log(pool);
-	    console.log(foo.totalCollateralETH.toString());
-	    console.log(foo.ltv.toString());
-	    console.log(" Balance Before ");
-	    console.log(balanceBefore.toString());
-	    console.log(arbiterBalanceBefore.toString());
+	    // working with the aave pool interface and fetching the data
+	    // user account data
+	    let foo = await pool.getUserAccountData(theBot.address);
+	    let reservesList = await pool.getReservesList();
+	    let addressesProvider = await pool.getAddressesProvider();
+	    // console.log(reservesList);
+	    console.log(addressesProvider);
+	    // console.log(foo);
+	    // console.log(foo.totalCollateralETH.toString());
+	    // console.log(foo.ltv.toString());
+	    // console.log(" Balance Before ");
+	    // console.log(balanceBefore.toString());
+	    // console.log(arbiterBalanceBefore.toString());
             const thousandDays = 1000 * 24 * 60 * 60;
             await hre.network.provider.request({
                 method: "evm_increaseTime",
@@ -52,11 +83,12 @@ describe("TheBot", function () {
 
         it('should provide the principal to the beneficiary', async () => {
             const balanceAfter = await ethers.provider.getBalance(beneficiary);
-	    console.log(" Balance After ");
-	    console.log(balanceAfter.toString());
+	    // console.log(" Balance After ");
+	    // console.log(balanceAfter.toString());
             const diff = balanceAfter.sub(balanceBefore);
             assert.equal(diff.toString(), deposit.toString());
         });
     });
+    */
 });
 
